9058500e0031382bb8068d61d51be2ea4a4287bc
Piotr Boruc <piotr.m.boruc@gmail.com>
2024-12-05 19:59:49 +0100

    attempt to follow OTEL logs specs

--- slog/slog.go
+++ slog/slog.go
@@ -2,26 +2,23 @@ package slog
 
 import (
 	"context"
+	"fmt"
 
 	"log/slog"
 
 	"go.opentelemetry.io/otel/codes"
+	otellog "go.opentelemetry.io/otel/log"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 )
-
-// The `const TraceIDKey = "TraceId"` line is declaring a constant variable named `TraceIDKey` with
-// the value `"TraceId"`. This constant is used as a key to add an attribute to a log record.
-const TraceIDKey = "TraceId"
-
-// The line `const SpanIDKey = "SpanId"` is declaring a constant variable named `SpanIDKey` with the
-// value `"SpanId"`. This constant is used as a key to add an attribute to a log record.
-const SpanIDKey = "SpanId"
 
 // The TracingHandler type is a wrapper around a slog.Handler.
 // @property handler - The `handler` property is a variable of type `slog.Handler`.
 type TracingHandler struct {
 	handler slog.Handler
 }
+
+const sevOffset = slog.Level(otellog.SeverityDebug) - slog.LevelDebug
 
 // The function NewTracingHandler creates a new TracingHandler by wrapping an existing slog.Handler.
 func NewTracingHandler(h slog.Handler) *TracingHandler {
@@ -53,10 +50,7 @@ func (h *TracingHandler) Handle(ctx context.Context, r slog.Record) error {
 			span.SetStatus(codes.Error, r.Message)
 		}
 
-		if spanCtx := span.SpanContext(); spanCtx.HasTraceID() {
-			r.AddAttrs(slog.String(TraceIDKey, spanCtx.TraceID().String()))
-			r.AddAttrs(slog.String(SpanIDKey, string(spanCtx.SpanID().String())))
-		}
+		r = alignWithOTELSpecs(r, span)
 	}
 
 	return h.handler.Handle(ctx, r)
@@ -74,4 +68,77 @@ func (h *TracingHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 // represents the name of the log group.
 func (h *TracingHandler) WithGroup(name string) slog.Handler {
 	return NewTracingHandler(h.handler.WithGroup(name))
+}
+
+// https://opentelemetry.io/docs/specs/otel/logs/data-model/#log-and-event-record-definition
+// Timestamp	Time when the event occurred.
+// ObservedTimestamp	Time when the event was observed.
+// TraceId	Request trace id.
+// SpanId	Request span id.
+// TraceFlags	W3C trace flag.
+// SeverityText	The severity text (also known as log level).
+// SeverityNumber	Numerical value of the severity.
+// Body	The body of the log record.
+// Resource	Describes the source of the log.
+// InstrumentationScope	Describes the scope that emitted the log.
+// Attributes	Additional information about the event.
+func alignWithOTELSpecs(r slog.Record, span trace.Span) slog.Record {
+	traceId := ""
+	spanId := ""
+	traceFlags := ""
+	if spanCtx := span.SpanContext(); spanCtx.HasTraceID() {
+		spanId = spanCtx.SpanID().String()
+		traceId = spanCtx.TraceID().String()
+		traceFlags = spanCtx.TraceFlags().String()
+	}
+	r.AddAttrs(slog.String("TraceId", traceId))
+	r.AddAttrs(slog.String("SpanId", spanId))
+	r.AddAttrs(slog.String("TraceFlags", traceFlags))
+
+	// Convert the span to ReadOnlySpan to access attributes
+	roSpan, ok := span.(sdktrace.ReadOnlySpan)
+	if !ok {
+		fmt.Println("Span is not a ReadOnlySpan")
+	} else {
+		// Create a group for span attributes
+		attributes := make([]any, 0) // Use []any for slog.Group compatibility
+		for _, attr := range roSpan.Attributes() {
+			attributes = append(attributes, slog.String(string(attr.Key), attr.Value.Emit()))
+		}
+		r.AddAttrs(slog.Group("Attributes", attributes...))
+
+		// Add InstrumentationScope details as a group
+		scope := roSpan.InstrumentationScope()
+		scopeAttrs := make([]any, 0) // Use []any for slog.Group compatibility
+		iter := scope.Attributes.Iter()
+		for iter.Next() {
+			attr := iter.Attribute()
+			scopeAttrs = append(scopeAttrs, slog.String(string(attr.Key), attr.Value.Emit()))
+		}
+		r.AddAttrs(slog.Group("InstrumentationScope",
+			slog.String("Name", scope.Name),
+			slog.String("Version", scope.Version),
+			slog.Group("Attributes", scopeAttrs...),
+		))
+
+		// Add other span details
+		r.AddAttrs(
+			slog.String("SpanName", roSpan.Name()),
+			slog.String("SpanKind", roSpan.SpanKind().String()),
+			slog.String("Resource", roSpan.Resource().String()),
+			slog.String("Timestamp", roSpan.StartTime().String()),
+			slog.String("ObservedTimestamp", roSpan.StartTime().String()),
+		)
+	}
+
+	sev := slog.Level(int(r.Level)) + sevOffset
+
+	// Add severity and message details
+	r.AddAttrs(
+		slog.String("SeverityText", r.Level.String()),
+		slog.Int("SeverityNumber", int(sev)),
+		slog.String("Body", r.Message),
+	)
+
+	return r
 }
